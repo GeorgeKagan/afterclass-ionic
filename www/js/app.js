c807aee3c77a1ee7342d28819a384032e0506b49
gosha <georgekagan@gmail.com>
2015-01-19 01:18:54 +0200

    use routing + design homepage posts

--- www/js/app.js
+++ www/js/app.js
@@ -23,49 +23,57 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
+  .state('home', {
+    url: "/home",
+    //abstract: true,
+    templateUrl: "templates/home.html",
+    controller: 'HomeCtrl'
   })
-
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
+  .state('askQuestion', {
+    url: "/askQuestion",
+    templateUrl: "templates/ask-question.html",
+    controller: 'AskQuestionCtrl'
   })
+  ;
+  //.state('app.search', {
+  //  url: "/search",
+  //  views: {
+  //    'menuContent': {
+  //      templateUrl: "templates/search.html"
+  //    }
+  //  }
+  //})
+  //
+  //.state('app.browse', {
+  //  url: "/browse",
+  //  views: {
+  //    'menuContent': {
+  //      templateUrl: "templates/browse.html"
+  //    }
+  //  }
+  //})
+  //  .state('app.playlists', {
+  //    url: "/playlists",
+  //    views: {
+  //      'menuContent': {
+  //        templateUrl: "templates/playlists.html",
+  //        controller: 'PlaylistsCtrl'
+  //      }
+  //    }
+  //  })
+  //
+  //.state('app.single', {
+  //  url: "/playlists/:playlistId",
+  //  views: {
+  //    'menuContent': {
+  //      templateUrl: "templates/playlist.html",
+  //      controller: 'PlaylistCtrl'
+  //    }
+  //  }
+  //})
 
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/home');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,42 +1,16 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ui.router'])
 
-.controller('MyCtrl', function($scope, $ionicScrollDelegate) {
+.controller('HomeCtrl', function($scope, $ionicScrollDelegate, $state) {
   $scope.items = [
-    {title: 'lol rofl lmao first'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao'},
-    {title: 'lol rofl lmao last'},
+    {title: 'lol rofl lmao first'}
   ];
+  $scope.askQuestion = function() {
+    //alert('aaa');
+    $state.go('askQuestion');
+  };
   $scope.gotScrolled = function() {
-    var y = angular.element('.scroll').offset().top;
+    var y = angular.element('.scroll:visible').offset().top;
     if (y <= -155) {
-      console.log('yes');
       //angular.element('#ac-tabs-outer').show();
       //angular.element('#ac-tabs-inner').hide();
       angular.element('#ac-tabs-inner .tabs').css('top', 44);
@@ -45,7 +19,6 @@ angular.module('starter.controllers', [])
       //angular.element('#ac-tabs-inner').show();
       var curr_y = angular.element('#ac-tabs-inner .tabs').position().top;
       angular.element('#ac-tabs-inner .tabs').css('top', 199 - Math.abs(y));
-      console.log('wat');
     }
   };
   $scope.scrollToTop = function() {
@@ -54,50 +27,56 @@ angular.module('starter.controllers', [])
     return true;
   };
 })
-
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+.controller('AskQuestionCtrl', function($scope, $ionicScrollDelegate, $state) {
+  $scope.backToHome = function() {
+    $state.go('home');
   };
 })
+;
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+//.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+//  // Form data for the login modal
+//  $scope.loginData = {};
+//
+//  // Create the login modal that we will use later
+//  $ionicModal.fromTemplateUrl('templates/login.html', {
+//    scope: $scope
+//  }).then(function(modal) {
+//    $scope.modal = modal;
+//  });
+//
+//  // Triggered in the login modal to close it
+//  $scope.closeLogin = function() {
+//    $scope.modal.hide();
+//  };
+//
+//  // Open the login modal
+//  $scope.login = function() {
+//    $scope.modal.show();
+//  };
+//
+//  // Perform the login action when the user submits the login form
+//  $scope.doLogin = function() {
+//    console.log('Doing login', $scope.loginData);
+//
+//    // Simulate a login delay. Remove this and replace with your login
+//    // code if using a login system
+//    $timeout(function() {
+//      $scope.closeLogin();
+//    }, 1000);
+//  };
+//})
+//
+//.controller('PlaylistsCtrl', function($scope) {
+//  $scope.playlists = [
+//    { title: 'Reggae', id: 1 },
+//    { title: 'Chill', id: 2 },
+//    { title: 'Dubstep', id: 3 },
+//    { title: 'Indie', id: 4 },
+//    { title: 'Rap', id: 5 },
+//    { title: 'Cowbell', id: 6 }
+//  ];
+//})
+//
+//.controller('PlaylistCtrl', function($scope, $stateParams) {
+//});
