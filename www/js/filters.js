813cade0b41219e727fa5054ea87e58cf034f9cc
gosha <georgekagan@gmail.com>
2015-03-13 14:40:47 +0200

    add nl2br + fix nowrap

--- www/js/filters.js
+++ www/js/filters.js
@@ -13,4 +13,9 @@ angular.module('afterclass.filters', [])
             return filtered;
         };
     })
+    .filter('nl2br', function($sce){
+        return function(text) {
+            return text ? $sce.trustAsHtml(text.replace(/\n/g, '<br>')) : '';
+        };
+    });
 ;
