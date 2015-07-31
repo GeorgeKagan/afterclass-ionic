32b5955dbcfea957cac5182e0eb49eeb1697bcdf
gosha <georgekagan@gmail.com>
2015-07-31 18:52:00 +0300

    side menu popover - make it dynamic links and height

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,4 +1,4 @@
-angular.module('afterclass.controllers').controller('AppCtrl', function ($scope, $rootScope, $ionicPopover, $state, $ionicHistory, $window, MyFirebase) {
+angular.module('afterclass.controllers').controller('AppCtrl', function ($scope, $rootScope, $ionicPopover, $state, $ionicHistory, $window, MyFirebase, User) {
     // Logout user
     $scope.logout = function () {
         var ref = MyFirebase.getRef();
@@ -10,6 +10,17 @@ angular.module('afterclass.controllers').controller('AppCtrl', function ($scope,
         $state.go('login');
     };
 
+    // Popover side menu
+    $scope.links = [
+        {sref: 'about', text: 'PAGES.ABOUT.MENU'},
+        {sref: 'contact', text: 'PAGES.CONTACT.MENU'}
+        //{sref: 'coupon', text: 'PAGES.COUPON.MENU'}
+    ];
+    User.getFromUsersCollection().then(function(user) {
+        if (!user.is_teacher) {
+            $scope.links.push({sref: 'getCredit', text: 'GET_POINTS'});
+        }
+    });
     $scope.hidePopover = function() {
         $scope.popover.hide();
     };
