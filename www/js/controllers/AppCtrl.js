17fcc1ba8dc82ebdd79c483ca617cac509c63004
gosha <georgekagan@gmail.com>
2015-08-07 18:09:47 +0300

    show firebase env in home + color dev actions

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,4 +1,7 @@
 angular.module('afterclass.controllers').controller('AppCtrl', function ($scope, $rootScope, $ionicPopover, $state, $ionicHistory, $window, MyFirebase, User) {
+
+    $rootScope.env = localStorage.getItem('env');
+
     // Logout user
     $scope.logout = function () {
         var ref = MyFirebase.getRef();
@@ -26,22 +29,24 @@ angular.module('afterclass.controllers').controller('AppCtrl', function ($scope,
         var devUsers = ['1591285834446649', '1518736295015643', '10205593403011749', '10205364847174667', '10153250113479854', '10152843702557886'],
             env = localStorage.getItem('env');
         if (_.indexOf(devUsers, $rootScope.user.id) !== -1) {
+            $rootScope.isDevUser = true;
             // Switch Firebase env
             $scope.links.push({onclick: function ($event) {
                 localStorage.setItem('env', env === 'dev' ? 'prod' : 'dev');
                 $scope.logout();
                 $event.preventDefault();
                 location.reload();
-            }, sref: 'dummy', text: 'Switch to Firebase ' + (env === 'dev' ? 'PROD' : 'DEV')});
+            }, sref: 'dummy', classes: 'assertive', text: 'Switch to Firebase ' + (env === 'dev' ? 'PROD' : 'DEV')});
             // Delete Firebase user
             $scope.links.push({onclick: function ($event) {
                 User.deleteUser();
                 $scope.logout();
                 $event.preventDefault();
                 location.reload();
-            }, sref: 'dummy', text: 'Delete Firebase User'});
+            }, sref: 'dummy', classes: 'assertive', text: 'Delete Firebase User'});
         }
     });
+
     $scope.hidePopover = function() {
         $scope.popover.hide();
     };
