7a0c246bf73a5dc182efb8acf89bfca5e392c394
gkagan <georgekagan@gmail.com>
2015-10-05 00:43:56 +0300

    minor tweaks

--- www/js/app.js
+++ www/js/app.js
@@ -13,7 +13,7 @@ angular.module('afterclass', ['ionic', 'afterclass.controllers', 'afterclass.dir
             }
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
+            if (window.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             }
             if (window.StatusBar) {
