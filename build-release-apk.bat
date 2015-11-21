099e7b6e71bf1099dbd16abd1e4b2a8670b3399a
gkagan <georgekagan@gmail.com>
2015-11-21 16:16:47 +0200

    update build script and change package name

--- build-release-apk.bat
+++ build-release-apk.bat
@@ -1,3 +1,3 @@
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms\android\build\outputs\apk\android-release-unsigned.apk alias_name
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms\android\build\outputs\apk\android-release-unsigned.apk afterclass
 
 zipalign -v 4 platforms\android\build\outputs\apk\android-release-unsigned.apk AfterClass.apk
