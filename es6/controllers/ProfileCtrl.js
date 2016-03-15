9ae2970908e31bc90e11ba31c8ec17e58c9d3a4c
George Kagan <georgekagan@gmail.com>
2016-03-15 19:43:28 +0200

    profile: change password for provider=password users (non facebook)

--- es6/controllers/ProfileCtrl.js
+++ es6/controllers/ProfileCtrl.js
@@ -1,3 +1,38 @@
-angular.module('afterclass.controllers').controller('ProfileCtrl', ($ionicTabsDelegate) => {
+angular.module('afterclass.controllers').controller('ProfileCtrl', ($rootScope, $scope, $ionicTabsDelegate, $ionicPopup, $translate, $ionicLoading, MyFirebase) => {
+
+    var ref = MyFirebase.getRef();
+
     $ionicTabsDelegate.select(0);
+
+    $scope.account = {oldPassword: '', newPassword: ''};
+
+    $scope.canChangePassword = () => $scope.account.oldPassword.trim() && $scope.account.newPassword.trim();
+
+    $scope.changePassword = () => {
+        $ionicLoading.show({template: '<ion-spinner class="spinner-calm"></ion-spinner>'});
+        ref.changePassword({
+            email      : $rootScope.user.email,
+            oldPassword: $scope.account.oldPassword,
+            newPassword: $scope.account.newPassword
+        }, function(error) {
+            if (error === null) {
+                $scope.account.oldPassword = '';
+                $scope.account.newPassword = '';
+                $ionicPopup.alert({
+                    title   : 'הצלחה',
+                    template: 'הסיסמה שונתה בהצלחה',
+                    okText  : $translate.instant('OK')
+                });
+                console.log("Password changed successfully");
+            } else {
+                $ionicPopup.alert({
+                    title   : 'שגיאה',
+                    template: 'הסיסמה הישנה שהקשת אינה נכונה',
+                    okText  : $translate.instant('OK')
+                });
+                console.log("Error changing password:", error);
+            }
+            $ionicLoading.hide();
+        });
+    };
 });
