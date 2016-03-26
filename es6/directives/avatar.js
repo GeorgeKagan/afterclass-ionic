9ab42aee20d54a880766377a3798480d42241dbb
gkagan <georgekagan@gmail.com>
2016-03-26 15:06:12 +0300

    use avatar directive

--- es6/directives/avatar.js
+++ es6/directives/avatar.js
@@ -1,13 +1,15 @@
 angular.module('afterclass.directives').directive('avatar', () => {
     return {
-        restrict: 'E',
+        restrict: 'AE',
+        transclude: true,
         template:
             `<img ng-if="userId.indexOf('facebook') > -1" 
                     ng-src="http://graph.facebook.com/{{::userId.replace('facebook:', '')}}/picture?type=square"
                     width="{{::size}}" height="{{::size}}">
              <img ng-if="userId.indexOf('facebook') === -1" 
                     ng-src="https://secure.gravatar.com/avatar/{{::userId}}?d=identicon" 
-                    width="{{::size}}" height="{{::size}}">`,
+                    width="{{::size}}" height="{{::size}}">
+             <div ng-transclude></div>`,
         scope: {
             userId: '=',
             size: '='
