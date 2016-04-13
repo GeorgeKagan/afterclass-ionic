bbddc3e75cb23c835364912f1fcfde1087e6a8fd
George Kagan <georgekagan@gmail.com>
2016-04-13 22:24:57 +0300

    delete commented out code (payment withdraw & onboarding ios push)

--- es6/controllers/GetPaymentCtrl.js
+++ es6/controllers/GetPaymentCtrl.js
@@ -9,40 +9,4 @@ angular.module('afterclass.controllers').controller('GetPaymentCtrl', ($rootScop
 
     /* DEBUG FEATURE*/
     //Payment._debugCreatePayment();
-
-    /*$scope.withdraw = function () {
-        if (window.cordova && !$cordovaNetwork.isOnline()) {
-            return alert($translate.instant('CHECK_INTERNET'));
-        }
-        var confirmPopup = $ionicPopup.confirm({
-            title: $translate.instant('PAYMENT.GET_MONEY'),
-            template: $translate.instant('FORM.SURE'),
-            cancelText: $translate.instant('CANCEL'),
-            okText: $translate.instant('PAYMENT.WITHDRAW')
-        });
-        confirmPopup.then(function(res) {
-            if (!res) {
-                return;
-            }
-            var balance = $scope.payment.balance;
-            $ionicLoading.show({template: '<ion-spinner class="spinner-calm"></ion-spinner>'});
-            $timeout(function () {
-                Payment.withdraw($scope.payment.balanceFbId).then(function () {
-                    $ionicLoading.hide();
-                    $('#gp-balance').slideUp(function() {
-                        $scope.$apply(function () {
-                            $scope.payment.balance = 0;
-                        });
-                        $(this).slideDown(function () {
-                            angular.element('.list li:first-child').addClass('highlight-new');
-                            $timeout(function() {
-                                angular.element('.list li:first-child').removeClass('highlight-new');
-                            }, 3000);
-                        });
-                    });
-                });
-            }, 1000);
-        });
-    };*/
-
 });
--- es6/controllers/OnBoardingCtrl.js
+++ es6/controllers/OnBoardingCtrl.js
@@ -3,40 +3,10 @@ angular.module('afterclass.controllers').controller('OnBoardingCtrl', ($scope, $
 
     $scope.next = () => {
         $ionicSlideBoxDelegate.next();
-
-       /* alert("asking");
-        var sns = new AWS.SNS({
-            region: 'us-west-2',
-            accessKeyId: 'AKIAIWZPMXE5FSUW7N6A',
-            secretAccessKey: '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'
-        });
-
-        var iosConfig = {
-            "badge": true,
-            "sound": true,
-            "alert": true
-        };
-
-        $cordovaPush.register(iosConfig).then(function(deviceToken) {
-            // Success -- send deviceToken to server, and store for future use
-            $log.log("deviceToken: " + deviceToken);
-            alert("deviceToken: " + deviceToken);
-            //$http.post("http://server.co/", {user: "Bob", tokenID: deviceToken})
-            var params = {
-                PlatformApplicationArn: 'arn:aws:sns:us-west-2:859437719678:app/APNS_SANDBOX/afterclass_dev',
-                Token: deviceToken
-            };
-            sns.createPlatformEndpoint(params, function(err, data) {
-
-                q.resolve(data.EndpointArn);
-            });
-        }, function(err) {
-            alert("Registration error: " + err)
-        });*/
     };
 
     $scope.finish = () => {
-    //    $ionicSlideBoxDelegate.previous();
+        //$ionicSlideBoxDelegate.previous();
         localStorage.setItem('finished_on_boarding', true);
         $ionicHistory.nextViewOptions({disableBack: true});
         $state.go('login');
