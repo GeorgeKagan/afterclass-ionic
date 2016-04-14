915c550ead9c8ba791c4ea40c65b32ade6225a5e
George Kagan <georgekagan@gmail.com>
2016-04-14 19:57:39 +0300

    fix algo call by using jsonp (to bypass cross origin)

--- es6/services/Utils.js
+++ es6/services/Utils.js
@@ -6,10 +6,11 @@ angular.module('afterclass.services').factory('Utils', ($rootScope, $http, $log)
     Utils.triggerAlgorithm = () => {
         let userId = encodeURI($rootScope.user.$id),
             ts = Math.floor(Date.now() / 1000);
-        $http.get(
-            `http://afterclass-966.appspot.com/StartAlgorithm?userId=${userId}&timestamp=${ts}`,
-            data => $log.info(data)
-        );
+        angular.element.ajax({
+            type: "GET",
+            dataType: 'jsonp',
+            url: `http://afterclass-966.appspot.com/StartAlgorithm?userId=${userId}&timestamp=${ts}`
+        });
     };
 
     return Utils;
