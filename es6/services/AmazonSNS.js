50b7dfcc0b5e20d49b22522bec66b43a2f3f1821
gkagan <georgekagan@gmail.com>
2016-04-23 17:17:24 +0300

    fix ios push notifications (forgot to update to new plugin's api)

--- es6/services/AmazonSNS.js
+++ es6/services/AmazonSNS.js
@@ -22,71 +22,53 @@ angular.module('afterclass.services').factory('AmazonSNS', ($rootScope, $cordova
         }
         let q = $q.defer();
 
-        if (ionic.Platform.isIOS()) {
-            let iosConfig = {
-                badge: true,
-                sound: true,
-                alert: true
-            };
-            $cordovaPush.register(iosConfig).then(deviceToken => {
-                // Success -- send deviceToken to server, and store for future use
-                $log.log('deviceToken: ' + deviceToken);
-                //$http.post('http://server.co/', {user: 'Bob', tokenID: deviceToken})
+        let push = PushNotification.init({
+            android: {
+                senderID: '580333274108',
+                icon: 'outline'
+            },
+            ios: {
+                icon: 'outline'
+            }
+        });
+
+        push.on('registration', function(notification) {
+            if (notification.registrationId.length > 0) {
+                $log.log('registration ID = ' + notification.registrationId);
+                // CREATE ENDPOINT
                 let params = {
-                    //PlatformApplicationArn: 'arn:aws:sns:us-west-2:859437719678:app/APNS_SANDBOX/afterclass_dev',
-                    PlatformApplicationArn: 'arn:aws:sns:us-west-2:859437719678:app/APNS/ios-production',
-                    Token                 : deviceToken
+                    PlatformApplicationArn: ionic.Platform.isIOS()
+                        ? 'arn:aws:sns:us-west-2:859437719678:app/APNS/ios-production'
+                        : 'arn:aws:sns:us-west-2:859437719678:app/GCM/afterclass-android',
+                    Token                 : notification.registrationId
                 };
                 sns.createPlatformEndpoint(params, (err, data) => {
-                    $log.log('got amazon APNS ' + data + ',' + err);
+                    $log.log('EndpointArn', data.EndpointArn);
                     q.resolve(data.EndpointArn);
                 });
-            }, err => $log.log('can\'t get APNS ' + err));
-        }
-        else if (ionic.Platform.isAndroid()) {
-            let push = PushNotification.init({
-                android: {
-                    senderID: '580333274108',
-                    icon: 'outline'
-                }
-            });
+            }
+        });
 
-            push.on('registration', function(notification) {
-                if (notification.registrationId.length > 0) {
-                    $log.log('registration ID = ' + notification.registrationId);
-                    // CREATE ENDPOINT
-                    let params = {
-                        PlatformApplicationArn: 'arn:aws:sns:us-west-2:859437719678:app/GCM/afterclass-android',
-                        Token                 : notification.registrationId
-                    };
-                    sns.createPlatformEndpoint(params, (err, data) => {
-                        $log.log('EndpointArn', data.EndpointArn);
-                        q.resolve(data.EndpointArn);
-                    });
-                }
+        push.on('notification', function(data) {
+            // data.message,
+            // data.title,
+            // data.count,
+            // data.sound,
+            // data.image,
+            // data.additionalData
+            //This is the actual push notification. Its format depends on the data model from the push server
+            $log.log('message', data);
+            $ionicPopup.alert({
+                title   : data.title,
+                template: data.message,
+                okText  : $translate.instant('OK')
             });
+        });
 
-            push.on('notification', function(data) {
-                // data.message,
-                // data.title,
-                // data.count,
-                // data.sound,
-                // data.image,
-                // data.additionalData
-                //This is the actual push notification. Its format depends on the data model from the push server
-                $log.log('message', data);
-                $ionicPopup.alert({
-                    title   : data.title,
-                    template: data.message,
-                    okText  : $translate.instant('OK')
-                });
-            });
-
-            push.on('error', function(e) {
-                // e.message
-                $log.log('GCM error = ' + e.message);
-            });
-        }
+        push.on('error', function(e) {
+            // e.message
+            $log.log('GCM error = ' + e.message);
+        });
 
         return q.promise;
     };
