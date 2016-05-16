82c63d393e375040602a1786290ffec18d895c76
George Kagan <georgekagan@gmail.com>
2016-05-16 17:26:14 +0300

    add crosswalk (android only) + remove window.reload on lang set (was causing a white screen on app open for new users)

--- es6/services/I18n.js
+++ es6/services/I18n.js
@@ -7,7 +7,6 @@ angular.module('afterclass.services').factory('I18n', ($rootScope, $window, $tra
         // If user changed language and local storage was cleared
         if ($rootScope.user.ui_lang && !localStorage.getItem(('uiLang'))) {
             localStorage.setItem('uiLang', $rootScope.user.ui_lang);
-            $window.location.reload(true);
         }
         moment.locale($translate.use());
         $rootScope.uiLang = $translate.use();
