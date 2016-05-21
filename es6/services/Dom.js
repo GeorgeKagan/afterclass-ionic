4e231b4252e7e35610957bdc8bb7ee1f6f05be74
Arik Liber <arik.liber@startapp.com>
2016-05-21 15:51:01 +0300

    Fix the undefined 'scrollDiv.offset().top' bug

--- es6/services/Dom.js
+++ es6/services/Dom.js
@@ -10,16 +10,23 @@ angular.module('afterclass.services').factory('Dom', ($rootScope, $ionicScrollDe
         tabs_top_pos: $rootScope.user.is_teacher && ionic.Platform.isIOS() ? 240 : 210,
         gotScrolled: () => {
             let tabs         = angular.element('#ac-tabs-inner > .tabs'),
-                scrollDiv    = angular.element('#ac-tabs-inner .scroll:visible'),
-                y            = scrollDiv.offset().top;
+                scrollDiv    = angular.element('#ac-tabs-inner .scroll:visible');
 
-            if (!scrollDiv.length) { return; }
+            if(scrollDiv.length) {
+                let scrollDivOffset = scrollDiv.offset();
+                if(typeof scrollDivOffset !== 'undefined' && scrollDivOffset.top !== 'undefined') {
+                    let topOffset = scrollDivOffset.top;
+                    // Fade in or out header solid bg color
+                    angular.element('.bar-header')[topOffset <= -15 ? 'addClass' : 'removeClass']('scrolled');
 
-            // Fade in or out header solid bg color
-            angular.element('.bar-header')[y <= -15 ? 'addClass' : 'removeClass']('scrolled');
-
-            // Tabs sticky on top or following scroll, respectively
-            tabs.css('top', y <= -167 ? Dom.homepageTabs.getHeaderSize() : Dom.homepageTabs.tabs_top_pos - Math.abs(y));
+                    // Tabs sticky on top or following scroll, respectively
+                    tabs.css('top', topOffset <= -167 ? Dom.homepageTabs.getHeaderSize() : Dom.homepageTabs.tabs_top_pos - Math.abs(topOffset));
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
         },
         scrollToTop: () => {
             $ionicScrollDelegate.scrollTop();
