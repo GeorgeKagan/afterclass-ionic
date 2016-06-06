70bafb7f7b2e31fda5666fb60a42aa0cf3f81f53
George Kagan <georgekagan@gmail.com>
2016-06-06 19:47:29 +0300

    change build-release-apk.bat (android) to use a crosswalk apk + some more shit

---
+++ READ-BEFORE-BUILD.md
@@ -0,0 +1,2 @@
+For android:
+* Make sure you set android:targetSdkVersion to 22 in android\AndroidManifest.xml (fix android 6 camera issue):
--- build-release-apk.bat
+++ build-release-apk.bat
@@ -1,3 +1,3 @@
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms\android\build\outputs\apk\android-release-unsigned.apk afterclass
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms\android\build\outputs\apk\android-armv7-release-unsigned.apk afterclass
 
-zipalign -v 4 platforms\android\build\outputs\apk\android-release-unsigned.apk AfterClass.apk
+zipalign -v 4 platforms\android\build\outputs\apk\android-armv7-release-unsigned.apk AfterClass.apk
