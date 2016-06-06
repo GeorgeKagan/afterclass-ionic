48d5c74e6abf98313f775bb9d202c4f9af4ff7ff
George Kagan <georgekagan@gmail.com>
2016-06-06 21:24:03 +0300

    remove read-before-build.md + ask for camera permission when using the camera (for and 6)

--- READ-BEFORE-BUILD.md
+++
@@ -1,2 +0,0 @@
-For android:
-* Make sure you set android:targetSdkVersion to 22 in android\AndroidManifest.xml (fix android 6 camera issue):
--- es6/services/MyCamera.js
+++ es6/services/MyCamera.js
@@ -14,27 +14,33 @@ angular.module('afterclass.services').factory('MyCamera', ($q, $window) => {
                 allowEdit           : false
             }, options);
 
-            navigator.camera.getPicture(result => {
-                $window.resolveLocalFileSystemURL(result, fileEntry => {
-                    fileEntry.file(fileObj => {
-                        // On some versions, gallery attachment url is bad, so let's fix it!
-                        if (result.substring(0, 21) === 'content://com.android') {
-                            let photo_split = result.split('%3A');
-                            result = 'content://media/external/images/media/' + photo_split[1];
-                        }
-                        let fileType = result.substring(result.lastIndexOf('.') + 1);
-                        let is_image = true;
+            cordova.plugins.diagnostic.requestCameraAuthorization(status => {
+                console.log('Authorization request for camera use was ' + (status == cordova.plugins.diagnostic.permissionStatus.GRANTED ? 'granted' : 'denied'));
+                navigator.camera.getPicture(result => {
+                    $window.resolveLocalFileSystemURL(result, fileEntry => {
+                        fileEntry.file(fileObj => {
+                            // On some versions, gallery attachment url is bad, so let's fix it!
+                            if (result.substring(0, 21) === 'content://com.android') {
+                                let photo_split = result.split('%3A');
+                                result = 'content://media/external/images/media/' + photo_split[1];
+                            }
+                            let fileType = result.substring(result.lastIndexOf('.') + 1),
+                                is_image = true;
 
-                        if (fileType != 'png' && fileType != 'jpg' && fileType != 'gif' && fileType != 'bmp') {
-                            is_image = angular.element.inArray(fileObj.type, ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']) > -1;
-                        }
-                        q.resolve({imageURI: result, is_image});
-                    });
-                }, error => reportError('Failed resolveLocalFileSystemURL: ' + error));
-            }, err => {
-                reportError('Failed camera.getPicture: ' + err);
-                q.reject(err);
-            }, options);
+                            if (fileType != 'png' && fileType != 'jpg' && fileType != 'gif' && fileType != 'bmp') {
+                                is_image = angular.element.inArray(fileObj.type, ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']) > -1;
+                            }
+                            q.resolve({imageURI: result, is_image});
+                        });
+                    }, error => reportError('Failed resolveLocalFileSystemURL: ' + error));
+                }, err => {
+                    reportError('Failed camera.getPicture: ' + err);
+                    q.reject(err);
+                }, options);
+            }, error => {
+                console.error(error);
+            });
+
             return q.promise;
         }
     };
