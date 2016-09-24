554cf05ec701a064cb5207ef6b9c96cb10e52669
George Kagan <georgekagan@gmail.com>
2016-09-24 21:19:02 +0300

    teacher q's - load only if acceptedBy===null + fix loader gif + separate home tabs' content into tpls

--- es6/services/PostsFetcher.js
+++ es6/services/PostsFetcher.js
@@ -7,20 +7,17 @@ angular.module('afterclass.services').factory('PostsFetcher', ($rootScope, $fire
 
         // Teacher
         getForTeacher: scope => {
-            let sync, postsTeacherAnswered;
-            // Unanswered posts for teacher (status = unanswered and local filter [if in potential teachers array])
-            // TODO: HIGHLY UN-SCALABLE (THINK OF A WAY TO FETCH ONLY IF IN POTENTIAL TEACHERS)
-            scope.posts_teacher_unanswered = $firebaseArray(ref);
+            // Unanswered posts for teacher (acceptedBy = null and local filter [if in potential teachers array])
+            let unanswered = $firebaseArray(ref.orderByChild('acceptedBy').equalTo(null));
+            unanswered.$loaded().then(() => scope.posts_teacher_unanswered = unanswered);
 
             // Answered posts by teacher (last_tutor_id = this teacher's id)
-            sync                 = ref.orderByChild('last_tutor_id').equalTo($rootScope.user.uid);
-            postsTeacherAnswered = $firebaseArray(sync);
-            postsTeacherAnswered.$loaded().then(() => scope.posts_teacher_answered = postsTeacherAnswered);
+            let answered = $firebaseArray(ref.orderByChild('last_tutor_id').equalTo($rootScope.user.uid));
+            answered.$loaded().then(() => scope.posts_teacher_answered = answered);
         },
         ifPotentialTeacher: post => {
-            // If post accepted by teacher and not by current teacher, exclude post
-            if (post.acceptedBy && post.acceptedBy !== $rootScope.user.uid) { return false; }
             let teacher_ids = [];
+            // Extract teacher_ids from potential_tutors array
             if (post.potential_tutors) {
                 _.each(post.potential_tutors, (item, id) => teacher_ids.push(id));
             }
